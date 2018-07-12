128c3b32cbddd9806c2e5751a429d388cfdabe53
mglennwithers <michael@charterbenefits.com>
2018-07-12 01:41:25 -0700

    test

--- DiscordRPGBot/php/events/messageEvent.php
+++ DiscordRPGBot/php/events/messageEvent.php
@@ -3,55 +3,58 @@ class MessageEvent
 {
     public static function HandleEvent(\CharlotteDunois\Yasmin\Models\Message $message)
     {
-        $guild = Guild::GetGuild($message->guild);
-        if($guild != null){Logger::Log('A guild was found.');}
-
-        $user = self::IdentifyUser($message);
-        if($user != null)
+        if(!$message->user->bot)
         {
-            Logger::Log('A user was found.');
-            if(substr($message->content, 0, 1) == $guild->settings['commandPrefix'])
+            $guild = Guild::GetGuild($message->guild);
+            if($guild != null){Logger::Log('A guild was found.');}
+
+            $user = self::IdentifyUser($message);
+            if($user != null)
             {
-                $content = explode(' ', substr($message->content, 1));
-                switch(strtolower($content[0]))
+                Logger::Log('A user was found.');
+                if(substr($message->content, 0, 1) == $guild->settings['commandPrefix'])
                 {
-                    case'hello':
-                        Logger::Log('Message event resovled as Greeting');
-                        return new Greeting();
-                        break;             
-                    case'changeprefix':
-                        Logger::Log('Message event resovled as ChangePrefix');
-                        $len = strlen($content[1]);
-                        if($len == 1)
-                        {
-                            $guild->settings['commandPrefix'] = $content[1];
-                            $message->channel->send('Prefix changed to ' . $content[1]);
-                            Logger::Log('Prefix changed to ' . $content[1]);
-                        }
-                        else if($len == 0)
-                        {
-                            $message->channel->send('No replacement prefix was sent');
-                            Logger::Log('No replacement prefix was sent');
-                        }
-                        else
-                        {
-                            $message->channel->send('New prefix was more than 1 character! Only 1 character prefixes are allowed.');
-                            Logger::Log('New prefix was more than 1 character! Only 1 character prefixes are allowed.');
-                        }
-                        break;
-                    case 'giveloot':
-                        $loot = new Loot();
-                        $item = $loot->GetLoot();
-                        $user->AddItem($item);
-                        return $item->Describe();
-                        break;
-                    case 'myinventory':
-                        return $user->ListInventory();
-                        break;
-                    default:
-                        Logger::Log('Message event resovled as Confusion');
-                        return new Confusion();
-                        break;
+                    $content = explode(' ', substr($message->content, 1));
+                    switch(strtolower($content[0]))
+                    {
+                        case'hello':
+                            Logger::Log('Message event resovled as Greeting');
+                            return new Greeting();
+                            break;             
+                        case'changeprefix':
+                            Logger::Log('Message event resovled as ChangePrefix');
+                            $len = strlen($content[1]);
+                            if($len == 1)
+                            {
+                                $guild->settings['commandPrefix'] = $content[1];
+                                $message->channel->send('Prefix changed to ' . $content[1]);
+                                Logger::Log('Prefix changed to ' . $content[1]);
+                            }
+                            else if($len == 0)
+                            {
+                                $message->channel->send('No replacement prefix was sent');
+                                Logger::Log('No replacement prefix was sent');
+                            }
+                            else
+                            {
+                                $message->channel->send('New prefix was more than 1 character! Only 1 character prefixes are allowed.');
+                                Logger::Log('New prefix was more than 1 character! Only 1 character prefixes are allowed.');
+                            }
+                            break;
+                        case 'giveloot':
+                            $loot = new Loot();
+                            $item = $loot->GetLoot();
+                            $user->AddItem($item);
+                            return $item->Describe();
+                            break;
+                        case 'myinventory':
+                            return $user->ListInventory();
+                            break;
+                        default:
+                            Logger::Log('Message event resovled as Confusion');
+                            return new Confusion();
+                            break;
+                    }
                 }
             }
         }
