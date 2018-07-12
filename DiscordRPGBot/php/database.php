848024ea99ae6a2253a840a3bdd4a293da825f26
mglennwithers <michael@charterbenefits.com>
2018-07-11 20:06:31 -0700

    test

--- DiscordRPGBot/php/database.php
+++ DiscordRPGBot/php/database.php
@@ -12,17 +12,18 @@ class Database
     protected static $pdo  = null;
 
     //this function returns a connection to the database
-    protected static function GetConnection(string $databaseName)
+    protected static function GetConnection()
     {
         $dsn = "mysql:host="    . self::$dbhost
             . ";port="          . self::$dbport
-            . ";dbname="        . $databaseName
-            . ";charset="       . Config::$database['charset'];
+            . ";dbname="        . self::$dbname
+            . ";charset="       . self::$charset;
         self::$pdo = new PDO($dsn, Config::$database['username'], Config::$database['password']);
         self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return self::$pdo;
     }
 
+    //returns the active pdo instance if it exists, otherwise creates one
     public static function PDO()
     {
         if(self::$pdo == null)
@@ -30,11 +31,16 @@ class Database
         else{return self::$pdo;}
     }
 
-    public static function Query(string $query, array $params)
+    //performs a database query
+    //@param string $query A database query ready to use in a prepared statement, uses :var syntax where variables occur
+    //@param array $params An associative array of parameter variables whose names match those in the query
+    //@param int $pdoFetchType PDO fetch style, defaults to PDO::FETCH_ASSOC
+    //@return returns the result of the query in the requested style
+    public static function Query(string $query, array $params = [], int $pdoFetchType = PDO::FETCH_ASSOC)
     {
         $preparedStatement = self::PDO()->prepare($query);
         $preparedStatement->execute($params);
-        $result = $preparedStatement->fetch(PDO::FETCH_ASSOC);
+        $result = $preparedStatement->fetch($pdoFetchType);
         return $result;
     }
 }
